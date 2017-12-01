7ed666d9dde74e969a26917489f1d42a327aeb7c
peerik <peter.eriksson@havochvatten.se>
2017-12-01 09:31:36 +0100

    Fixed NullPointerException when NAF input contains only CFR

--- service/src/test/java/eu/europa/ec/fisheries/uvms/plugins/naf/mapper/NafMessageResponseMapperTest.java
+++ service/src/test/java/eu/europa/ec/fisheries/uvms/plugins/naf/mapper/NafMessageResponseMapperTest.java
@@ -11,34 +11,35 @@ copy of the GNU General Public License along with the IFDM Suite. If not, see <h
  */
 package eu.europa.ec.fisheries.uvms.plugins.naf.mapper;
 
-import eu.europa.ec.fisheries.schema.exchange.movement.asset.v1.AssetIdType;
-import eu.europa.ec.fisheries.schema.exchange.movement.v1.MovementBaseType;
-import eu.europa.ec.fisheries.schema.exchange.movement.v1.MovementPoint;
-import eu.europa.ec.fisheries.schema.exchange.movement.v1.MovementType;
-import eu.europa.ec.fisheries.schema.exchange.movement.v1.SetReportMovementType;
-import eu.europa.ec.fisheries.schema.exchange.plugin.types.v1.PluginType;
-import eu.europa.ec.fisheries.uvms.exchange.model.util.DateUtils;
-import eu.europa.ec.fisheries.uvms.plugins.naf.*;
-import eu.europa.ec.fisheries.uvms.plugins.naf.constants.NafCodes;
-import eu.europa.ec.fisheries.uvms.plugins.naf.exception.PluginException;
-import static eu.europa.ec.fisheries.uvms.plugins.naf.mapper.NafMessageResponseMapper.dateString;
-import eu.europa.ec.fisheries.uvms.plugins.naf.util.DateUtil;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
-import java.text.ParseException;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import junit.framework.Assert;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import eu.europa.ec.fisheries.schema.exchange.movement.asset.v1.AssetIdType;
+import eu.europa.ec.fisheries.schema.exchange.movement.v1.MovementBaseType;
+import eu.europa.ec.fisheries.schema.exchange.movement.v1.MovementPoint;
+import eu.europa.ec.fisheries.schema.exchange.movement.v1.SetReportMovementType;
+import eu.europa.ec.fisheries.schema.exchange.plugin.types.v1.PluginType;
+import eu.europa.ec.fisheries.uvms.plugins.naf.constants.NafCodes;
+import eu.europa.ec.fisheries.uvms.plugins.naf.exception.PluginException;
+import eu.europa.ec.fisheries.uvms.plugins.naf.util.DateUtil;
+
 /**
  * Unit test for simple App.
  */
 public class NafMessageResponseMapperTest {
+	
+    private static final double DELTA_VALUE = 0.001;
 
     @BeforeClass
     public static void setUpClass() {
@@ -66,10 +67,10 @@ public class NafMessageResponseMapperTest {
             String pluginName = "NafMapperTest";
             SetReportMovementType retVal = NafMessageResponseMapper.mapToMovementType(nafMessage, pluginName);
 
-            Assert.assertEquals(pluginName, retVal.getPluginName());
-            Assert.assertEquals(PluginType.NAF, retVal.getPluginType());
-            Assert.assertNotNull(retVal.getTimestamp());
-            Assert.assertNotNull(retVal.getMovement());
+            assertEquals(pluginName, retVal.getPluginName());
+            assertEquals(PluginType.NAF, retVal.getPluginType());
+            assertNotNull(retVal.getTimestamp());
+            assertNotNull(retVal.getMovement());
         } catch (PluginException ex) {
             Logger.getLogger(NafMessageResponseMapperTest.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -81,47 +82,47 @@ public class NafMessageResponseMapperTest {
 
         String[] course = {NafCodes.COURSE, "10.5"};
         NafMessageResponseMapper.mapEntry(course, movement);
-        Assert.assertEquals(10.5D, movement.getReportedCourse());
+		assertEquals(10.5D, movement.getReportedCourse().doubleValue(), DELTA_VALUE);
 
         String[] speed = {NafCodes.SPEED, "70"};
         NafMessageResponseMapper.mapEntry(speed, movement);
-        Assert.assertEquals(7.0D, movement.getReportedSpeed());
+        assertEquals(7.0D, movement.getReportedSpeed().doubleValue(), DELTA_VALUE);
 
         String[] date = {NafCodes.DATE, "20160208"};
         NafMessageResponseMapper.mapEntry(date, movement);
-        Assert.assertEquals("20160208", NafMessageResponseMapper.dateString);
+        assertEquals("20160208", NafMessageResponseMapper.dateString);
 
         String[] time = {NafCodes.TIME, "1527"};
         NafMessageResponseMapper.mapEntry(time, movement);
-        Assert.assertEquals("1527", NafMessageResponseMapper.timeString);
+        assertEquals("1527", NafMessageResponseMapper.timeString);
 
         String[] externalMark = {NafCodes.EXTERNAL_MARK, "ABC-123"};
         NafMessageResponseMapper.mapEntry(externalMark, movement);
-        Assert.assertEquals("ABC-123", movement.getExternalMarking());
+        assertEquals("ABC-123", movement.getExternalMarking());
 
         String[] flag = {NafCodes.FLAG, "SWE"};
         NafMessageResponseMapper.mapEntry(flag, movement);
-        Assert.assertEquals("SWE", movement.getFlagState());
+        assertEquals("SWE", movement.getFlagState());
 
         String[] latN = {NafCodes.LATITUDE, "N1213"};
         NafMessageResponseMapper.mapEntry(latN, movement);
-        Assert.assertEquals(12.2167D, movement.getPosition().getLatitude());
+        assertEquals(12.2167D, movement.getPosition().getLatitude().doubleValue(), DELTA_VALUE);
 
         String[] latS = {NafCodes.LATITUDE, "S1213"};
         NafMessageResponseMapper.mapEntry(latS, movement);
-        Assert.assertEquals(-12.2167D, movement.getPosition().getLatitude());
+        assertEquals(-12.2167D, movement.getPosition().getLatitude().doubleValue(), DELTA_VALUE);
 
         String[] lonN = {NafCodes.LONGITUDE, "E1417"};
         NafMessageResponseMapper.mapEntry(lonN, movement);
-        Assert.assertEquals(14.2833D, movement.getPosition().getLongitude());
+        assertEquals(14.2833D, movement.getPosition().getLongitude().doubleValue(), DELTA_VALUE);
 
         String[] lonS = {NafCodes.LONGITUDE, "W1417"};
         NafMessageResponseMapper.mapEntry(lonS, movement);
-        Assert.assertEquals(-14.2833D, movement.getPosition().getLongitude());
+        assertEquals(-14.2833D, movement.getPosition().getLongitude().doubleValue(), DELTA_VALUE);
 
         String[] callSign = {NafCodes.RADIO_CALL_SIGN, "SMIT"};
         NafMessageResponseMapper.mapEntry(callSign, movement);
-        Assert.assertEquals("SMIT", movement.getAssetId().getAssetIdList().get(0).getValue());
+        assertEquals("SMIT", movement.getAssetId().getAssetIdList().get(0).getValue());
     }
 
     @Test
@@ -132,29 +133,29 @@ public class NafMessageResponseMapperTest {
         Date date = DateUtil.parseToUTCDateTime("20160208" + " " + "1558 UTC");
 
         NafMessageResponseMapper.mapDateTime(movement);
-        Assert.assertEquals(movement.getPositionTime(), date);
+        assertEquals(movement.getPositionTime(), date);
 
         movement.setPositionTime(null);
         NafMessageResponseMapper.timeString = null;
         NafMessageResponseMapper.mapDateTime(movement);
-        Assert.assertNull(movement.getPositionTime());
+        assertNull(movement.getPositionTime());
 
         movement.setPositionTime(null);
         NafMessageResponseMapper.timeString = "";
         NafMessageResponseMapper.mapDateTime(movement);
-        Assert.assertNull(movement.getPositionTime());
+        assertNull(movement.getPositionTime());
 
         NafMessageResponseMapper.timeString = "1558";
 
         movement.setPositionTime(null);
         NafMessageResponseMapper.dateString = null;
         NafMessageResponseMapper.mapDateTime(movement);
-        Assert.assertNull(movement.getPositionTime());
+        assertNull(movement.getPositionTime());
 
         movement.setPositionTime(null);
         NafMessageResponseMapper.dateString = "";
         NafMessageResponseMapper.mapDateTime(movement);
-        Assert.assertNull(movement.getPositionTime());
+        assertNull(movement.getPositionTime());
     }
 
     @Test
@@ -162,31 +163,31 @@ public class NafMessageResponseMapperTest {
         MovementBaseType movement = new MovementBaseType();
         NafMessageResponseMapper.mapIRCS("SMIT", movement);
 
-        Assert.assertNotNull(movement.getAssetId());
-        Assert.assertNotNull(movement.getAssetId().getAssetIdList());
-        Assert.assertEquals(1, movement.getAssetId().getAssetIdList().size());
-        Assert.assertEquals("SMIT", movement.getAssetId().getAssetIdList().get(0).getValue());
-        Assert.assertEquals(AssetIdType.IRCS, movement.getAssetId().getAssetIdList().get(0).getIdType());
+        assertNotNull(movement.getAssetId());
+        assertNotNull(movement.getAssetId().getAssetIdList());
+        assertEquals(1, movement.getAssetId().getAssetIdList().size());
+        assertEquals("SMIT", movement.getAssetId().getAssetIdList().get(0).getValue());
+        assertEquals(AssetIdType.IRCS, movement.getAssetId().getAssetIdList().get(0).getIdType());
     }
 
     @Test
     public void mapCFRTest() {
-        //MovementBaseType movement = new MovementBaseType();
-        //movement.setInternalReferenceNumber("SWE000008121");
-        //NafMessageResponseMapper.mapCFR("SWE000008121", movement);
+        MovementBaseType movement = new MovementBaseType();
+        NafMessageResponseMapper.mapCFR("SWE000008121", movement);
 
-        //Assert.assertNotNull(movement.getAssetId());
-        //Assert.assertNotNull(movement.getAssetId().getAssetIdList());
-        //Assert.assertEquals(1, movement.getAssetId().getAssetIdList().size());
-        //Assert.assertEquals(1, movement.getAssetId().getAssetIdList().size());
-       // Assert.assertEquals("SWE000008121", movement.getAssetId().getAssetIdList().get(0).getValue());
-        //Assert.assertEquals(AssetIdType.CFR, movement.getAssetId().getAssetIdList().get(0).getIdType());
+        assertNotNull(movement.getAssetId());
+        assertNotNull(movement.getAssetId().getAssetIdList());
+        assertEquals(1, movement.getAssetId().getAssetIdList().size());
+        assertEquals(1, movement.getAssetId().getAssetIdList().size());
+        assertEquals("SWE000008121", movement.getInternalReferenceNumber());
+        assertEquals("SWE000008121", movement.getAssetId().getAssetIdList().get(0).getValue());
+        assertEquals(AssetIdType.CFR, movement.getAssetId().getAssetIdList().get(0).getIdType());
     }
 
     @Test
     public void getMovementPointTest() {
         MovementPoint point = NafMessageResponseMapper.getMovementPoint(new MovementBaseType());
-        Assert.assertNotNull(point);
+        assertNotNull(point);
     }
 
     @Test
@@ -195,6 +196,6 @@ public class NafMessageResponseMapperTest {
         String speed = "105";
         NafMessageResponseMapper.mapSpeed(movement, speed);
 
-        Assert.assertEquals(10.5, movement.getReportedSpeed());
+        assertEquals(10.5, movement.getReportedSpeed().doubleValue(), DELTA_VALUE);
     }
 }
